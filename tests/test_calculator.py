8399b2bb47f64f86ca82f247b808a3699e8125cb
sheilaapr <230605110005@student.uin-malang.ac.id>
2026-04-29 15:39:07 +0700

    fix import path2

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,7 +1,7 @@
 import sys
 import os
 
-sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 import pytest
 from calculator import add, divide
